ad9c54c0cf1e34d994d21c4f34e7244cfbf9cd30
Waverley Leung <waverley.nyc@gmail.com>
2020-06-04 21:04:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,22 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+//return a new array and not modify the original.
 function addElementToBeginningOfArray(array, element) {
   addBeginArray = [element, ... array]
 }
 
+//alter the original array that's passed in
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  console.log(array)
 }
 
+//should not alter the original array
 function addElementToEndOfArray(array, element) {
   addEndArray = [...array, element]
 }
 
+//should alter the original array
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
 }
