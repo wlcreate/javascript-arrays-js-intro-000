0c2467714131e41cd032ca791f7b1763958ef6b5
Waverley Leung <waverley.nyc@gmail.com>
2020-06-04 21:10:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 //return a new array and not modify the original.
 function addElementToBeginningOfArray(array, element) {
   addBeginArray = [element, ... array]
+  return addBeginArray
 }
 
 //alter the original array that's passed in
