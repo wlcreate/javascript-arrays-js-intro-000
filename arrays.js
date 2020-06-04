388b413e2b764aef343b76df6d7a39ad07e534f3
Waverley Leung <waverley.nyc@gmail.com>
2020-06-04 20:51:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  addArray = [element, ... array]
+  addBeginArray = [element, ... array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  addEndArray = [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
 }
